17113e705d265f078a69f6f18b017fec876e451d
ShawnG <shawngway@gmail.com>
2019-10-23 17:59:49 -0400

    connections

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+});
+
+connection.connect((err)=> {
+    if(err){
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Krabby Patty');
+INSERT INTO burgers (burger_name) VALUES ('Big Double cookout style');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jr. Bacon Cheese Burger', true);
