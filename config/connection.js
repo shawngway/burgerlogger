80641c9369ef9d18797143f38a71babc40515ad3
ShawnG <shawngway@gmail.com>
2019-11-15 15:15:55 -0500

    heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,25 @@
 const mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: `localhost`,
+        user: `root`,
+        password: `hacktheplanet`,
+        database: `todoagain_db`
+    });
+};
 
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 8889,
+//     user: "root",
+//     password: "root",
+//     database: "burgers_db"
+// });
 
 connection.connect((err)=> {
     if(err){
